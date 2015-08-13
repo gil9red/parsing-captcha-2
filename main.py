81cd9df01887642dc84d6955a752a0a485e430ef
Ilya Petrash <ip1992@inbox.ru>
2015-08-14 03:29:18 +0500

    Первая проба

--- main.py
+++ main.py
@@ -3,23 +3,60 @@
 
 __author__ = 'ipetrash'
 
-from phone_img_parser import PhoneImgParser
-from glob import glob
+# from phone_img_parser import PhoneImgParser
+# from glob import glob
+#
+#
+# def num_im(file):
+#     file = file.replace('examples\\', '')
+#     file = file.replace('.png', '')
+#     return int(file)
+#
+#
+# if __name__ == '__main__':
+#     parser = PhoneImgParser()
+#
+#     # glob сортирует файлы как строки, а мне хотелось бы, чтобы сортировалось
+#     # по номеру в файле
+#     files = sorted(glob('examples/*.png'), key=num_im)
+#
+#     for file in files:
+#         phone = parser.parse_from_file(file)
+#         print('"{}" -- {}'.format(phone, file))
+
+from PIL import Image
 
 
-def num_im(file):
-    file = file.replace('examples\\', '')
-    file = file.replace('.png', '')
-    return int(file)
+num = 2
 
 
-if __name__ == '__main__':
-    parser = PhoneImgParser()
+im = Image.open('examples/{}.png'.format(num))
+# print(im)
+# print(im.histogram())
+print({i: c for i, c in enumerate(im.histogram()) if c != 0})
 
-    # glob сортирует файлы как строки, а мне хотелось бы, чтобы сортировалось
-    # по номеру в файле
-    files = sorted(glob('examples/*.png'), key=num_im)
+im = im.convert('L')
+w, h = im.size
 
-    for file in files:
-        phone = parser.parse_from_file(file)
-        print('"{}" -- {}'.format(phone, file))
+BLACK_PXL = 0
+WHITE_PXL = 255
+
+for x in range(w):
+    for y in range(h):
+        pxl = im.getpixel((x, y))
+        if pxl != BLACK_PXL and pxl != WHITE_PXL:
+            im.putpixel((x, y), 255)
+
+im.save('_{}.png'.format(num))
+print()
+# print(im.histogram())
+print({i: c for i, c in enumerate(im.histogram()) if c != 0})
+
+
+# for i in range(1, 6):
+#     im = Image.open('examples/{}.png'.format(i)).convert('L')
+#     # print(im)
+#     # print(im.histogram())
+#     print(Counter(im.histogram()))
+
+
